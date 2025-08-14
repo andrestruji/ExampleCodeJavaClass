9cad7147100059eb80d52b63658fc5f92e0fc60a
andrestruji <andrestruji40@gmail.com>
2025-08-13 23:16:38 -0600

    Actualizacion doc operaciones

--- Operaciones.java
+++ Operaciones.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class Operaciones {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
 
-        int operacion;
+        int operacion = 0;
         double numUno;
         double numDos;
         double res;
@@ -15,43 +15,62 @@ public class Operaciones {
         System.out.println("**********************************");
         System.out.println();
 
-        System.out.println("1- Suma");
-        System.out.println("2- Resta");
-        System.out.println("3- Multiplicacion");
-        System.out.println("4- Division");
-        System.out.println();
+        while (operacion != 5) {
 
-        System.out.println("Ingresa la opcion seleccionada: ");
-        operacion = entrada.nextInt();
-        System.out.println();
+            System.out.println("1- Suma");
+            System.out.println("2- Resta");
+            System.out.println("3- Multiplicacion");
+            System.out.println("4- Division");
+            System.out.println("5- Salir");
+            System.out.println();
 
-        System.out.println("Ingresa el primer numero");
-        numUno = entrada.nextDouble();
-        System.out.println();
+            System.out.println("Ingresa la opcion seleccionada: ");
+            operacion = entrada.nextInt();
+            System.out.println();
 
-        System.out.println("Ingresa el numero dos");
-        numDos = entrada.nextDouble();
+            if (operacion >= 1 && operacion <= 4) {
 
-        switch (operacion) {
-            case 1:
-                res = numUno + numDos;
-                System.out.println("El resultado de tu suma es: " + res);        
-                break;
-            case 2:
-                res = numUno - numDos;
-                System.out.println("El resultado de la resta es: " + res);
-                break;
-            case 3:
-                res = numUno * numDos;
-                System.out.println("El resultado de la multiplicacion es: " + res);
-                break;
-            case 4: 
-                res = numUno / numDos;
-                System.out.println("El resultado de la division es: " + res);
-                break;    
-            default:
-                System.out.println("Sistema cerrado");
-                break;
+                System.out.println("Ingresa el primer numero");
+                numUno = entrada.nextDouble();
+                System.out.println();
+
+                System.out.println("Ingresa el numero dos");
+                numDos = entrada.nextDouble();
+                System.out.println();
+
+                switch (operacion) {
+                    case 1:
+                        res = numUno + numDos;
+                        System.out.println("El resultado de tu suma es: " + res);
+                        System.out.println();
+                        break;
+                    case 2:
+                        res = numUno - numDos;
+                        System.out.println("El resultado de la resta es: " + res);
+                        System.out.println();
+                        break;
+                    case 3:
+                        res = numUno * numDos;
+                        System.out.println("El resultado de la multiplicacion es: " + res);
+                        System.out.println();
+                        break;
+                    case 4:
+                        res = numUno / numDos;
+                        System.out.println("El resultado de la division es: " + res);
+                        System.out.println();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            System.out.println("Ingresa un numero del 1 al 4 para continuar o un 5 para salir: ");
+            operacion = entrada.nextInt();
+            System.out.println();
         }
+
+        System.out.println("************************");
+        System.out.println("* Saliste del programa *");
+        System.out.println("************************");
     }
 }
